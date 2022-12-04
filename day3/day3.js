2f8ad60de5102128ed9c057dd2bb5c33d3df35e4
grant.wylie <grant@edgesource.net>
2022-12-03 23:45:43 -0800

    understand how this problem is solved now

--- day3/day3.js
+++ day3/day3.js
@@ -1,21 +1,28 @@
+"use strict";
+exports.__esModule = true;
 var fs3 = require('fs');
-var input = fs3.readFileSync('../day3input.txt', 'utf8');
-var inputArray = input.split('\n');
+var text_input3 = require('../utils/loadText.js');
+var day3_data = text_input3.getInputArray('day3input.txt');
 var priorities = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var chars = [];
 var score = 0;
-inputArray.forEach(function (line) {
-    var first = line.slice(0, line.length / 2);
-    var second = line.slice(line.length / 2, line.length);
-    for (var _i = 0, first_1 = first; _i < first_1.length; _i++) {
-        var c = first_1[_i];
-        if (second.includes(c)) {
-            chars.push(c);
+day3_data.forEach(function (line) {
+    // split each line into two equal halves
+    var half1 = line.slice(0, line.length / 2);
+    var half2 = line.slice(line.length / 2, line.length);
+    for (var _i = 0, half1_1 = half1; _i < half1_1.length; _i++) {
+        var char = half1_1[_i];
+        // see if the character is in the second half
+        if (half2.includes(char)) {
+            // if it is, add it to the array
+            chars.push(char);
             break;
         }
     }
 });
-chars.forEach(function (c) {
+// add up the scrore 
+for (var _i = 0, chars_1 = chars; _i < chars_1.length; _i++) {
+    var c = chars_1[_i];
     score += priorities.indexOf(c) + 1;
-});
+}
 console.log(score);
--- day3/day3.ts
+++ day3/day3.ts
@@ -1,26 +1,34 @@
+import { getInputArray } from "../utils/loadText";
+
+
 const fs3 = require('fs');
 
-const input = fs3.readFileSync('../day3input.txt', 'utf8');
-const inputArray = input.split('\n');
+const text_input3 = require('../utils/loadText.js');
+
+const day3_data = text_input3.getInputArray('day3input.txt');
 
 const priorities = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const chars: string[] = [];
 let score = 0;
 
-inputArray.forEach((line) => {
-	const first = line.slice(0, line.length / 2);
-	const second = line.slice(line.length / 2, line.length);
+day3_data.forEach((line) => {
+	// split each line into two equal halves
+    const half1 = line.slice(0, line.length / 2);
+    const half2 = line.slice(line.length / 2, line.length);
 
-	for (const c of first) {
-		if (second.includes(c)) {
-			chars.push(c);
-			break;
-		}
-	}
+    for (const char of half1) {
+        // see if the character is in the second half
+        if (half2.includes(char)) {
+            // if it is, add it to the array
+            chars.push(char);
+            break
+        }
+    }
 });
 
-chars.forEach((c) => {
-	score += priorities.indexOf(c) + 1;
-});
+// add up the scrore 
+for (const c of chars) {
+    score += priorities.indexOf(c) + 1;
+}
 
 console.log(score);
